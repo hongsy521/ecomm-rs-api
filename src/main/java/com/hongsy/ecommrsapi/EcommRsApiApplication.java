08369af2273fd3032a4e42f114e65e276ed6eb7d
hongsy521 <seoyeong0521@naver.com>
2025-11-17 22:03:00 +0900

    initial setting

---
+++ src/main/java/com/hongsy/ecommrsapi/EcommRsApiApplication.java
@@ -0,0 +1,13 @@
+package com.hongsy.ecommrsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommRsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommRsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hongsy/ecommrsapi/EcommRsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongsy.ecommrsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommRsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
