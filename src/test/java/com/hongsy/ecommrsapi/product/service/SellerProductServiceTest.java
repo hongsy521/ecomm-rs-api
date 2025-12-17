327772e9f2d143e605a0b9036a9b5471761e9765
hongsy521 <seoyeong0521@naver.com>
2025-12-18 00:01:12 +0900

    feat : SellerProductServiceTest -> editProduct에 대한 테스트 코드 success/NotFoundProduct/IncorrectSeller 경우 작성

--- src/test/java/com/hongsy/ecommrsapi/product/service/SellerProductServiceTest.java
+++ src/test/java/com/hongsy/ecommrsapi/product/service/SellerProductServiceTest.java
@@ -1,6 +1,7 @@
 package com.hongsy.ecommrsapi.product.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.any;
 import static org.mockito.Mockito.times;
@@ -10,8 +11,11 @@ import com.hongsy.ecommrsapi.product.dto.ProductRequestDto;
 import com.hongsy.ecommrsapi.product.dto.ProductResponseDto;
 import com.hongsy.ecommrsapi.product.entity.Product;
 import com.hongsy.ecommrsapi.product.repository.ProductRepository;
+import com.hongsy.ecommrsapi.util.exception.CustomException;
+import com.hongsy.ecommrsapi.util.exception.ErrorCode;
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -63,6 +67,101 @@ class SellerProductServiceTest {
             .contains("기모","블랙");
 
         verify(productRepository,times(1)).save(any(Product.class));
+    }
+
+    @Test
+    @DisplayName("상품 수정 성공 테스트 : 상품 정보 수정 확인")
+    void testEditProductSuccess(){
+        Long sellerId=24L;
+        Long productId=25L;
+
+        Product mockProduct = Product.builder()
+            .id(productId)
+            .name("보트넥 긴팔티")
+            .brandName("릿킴")
+            .info("겨울 전용 기모 긴팔T 입니다.")
+            .price(new BigDecimal(36000))
+            .image("url")
+            .colorGroup("검정색")
+            .tags(List.of("기모", "긴팔T", "보트넥", "블랙"))
+            .orderAmountFor30d(0L)
+            .stockQuantity(300)
+            .avgReviewScore(0.0)
+            .sellerId(sellerId)
+            .likeCount(0)
+            .build();
+
+        // given
+        ProductRequestDto editRequestDto = ProductRequestDto.builder()
+            .name("패딩 점퍼")
+            .brandName("릿킴")
+            .info("한겨울에도 입을 수 있는 패딩 점퍼 입니다.")
+            .price(new BigDecimal(123000))
+            .image("url")
+            .colorGroup("카키색")
+            .tags(List.of("한겨울", "패딩", "점퍼", "카키색","따뜻함"))
+            .stockQuantity(500)
+            .build();
+
+        given(productRepository.findById(productId)).willReturn(Optional.of(mockProduct));
+
+        // when
+        ProductResponseDto responseDto = sellerProductService.editProduct(sellerId,productId,editRequestDto);
+
+        // then
+        assertThat(responseDto).isNotNull();
+        assertThat(responseDto.getBrandName()).isEqualTo("릿킴");
+        assertThat(responseDto.getStockQuantity()).isEqualTo(500);
+        assertThat(responseDto.getTags())
+            .isNotNull()
+            .hasSize(5)
+            .contains("따뜻함","한겨울");
+
+
+        verify(productRepository,times(1)).findById(productId);
+        verify(productRepository,times(1)).save(any(Product.class));
+    }
+
+    @Test
+    @DisplayName("상품 수정 실패 테스트 : 상품 아이디가 존재하지 않는 경우 NOT_FOUND_PRODUCT 예외 발생 확인")
+    void testEditProductNotFound(){
+        Long sellerId=24L;
+        Long nonExistingProductId=25L;
+
+        ProductRequestDto requestDto = ProductRequestDto.builder().build();
+
+        given(productRepository.findById(nonExistingProductId)).willReturn(Optional.empty());
+
+        CustomException exception = assertThrows(CustomException.class,()->sellerProductService.editProduct(sellerId,nonExistingProductId,requestDto));
+
+        assertThat(exception.getMessage()).isEqualTo("상품을 찾을 수 없습니다.");
+
+        verify(productRepository,times(1)).findById(nonExistingProductId);
+        verify(productRepository,times(0)).save(any(Product.class));
+    }
+
+    @Test
+    @DisplayName("상품 수정 실패 테스트 : 상품의 소유자가 아닌 경우 INCORRECT_SELLER 예외 발생 확인")
+    void testEditProductIncorrectSeller(){
+        Long sellerId=24L;
+        Long incorrectSellerId=23L;
+        Long productId=25L;
+
+        Product mockProduct = Product.builder()
+            .id(productId)
+            .sellerId(sellerId)
+            .build();
+
+        ProductRequestDto requestDto = ProductRequestDto.builder().build();
+
+        given(productRepository.findById(productId)).willReturn(Optional.of(mockProduct));
+
+        CustomException exception = assertThrows(CustomException.class,()-> sellerProductService.editProduct(incorrectSellerId,productId,requestDto));
+
+        assertThat(exception.getMessage()).isEqualTo("판매자가 일치하지 않습니다.");
+
+        verify(productRepository,times(1)).findById(productId);
+        verify(productRepository,times(0)).save(any(Product.class));
     }
 
 }
