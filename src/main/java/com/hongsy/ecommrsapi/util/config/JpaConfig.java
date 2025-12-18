6354f7f0d503dccc4438501b2f6d308e7c8ef290
hongsy521 <seoyeong0521@naver.com>
2025-12-18 22:39:26 +0900

    test : webMvcTest를 위한 @EnableJpaAuditing 설정 분리

---
+++ src/main/java/com/hongsy/ecommrsapi/util/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package com.hongsy.ecommrsapi.util.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+}
